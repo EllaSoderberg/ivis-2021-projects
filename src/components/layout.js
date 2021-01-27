7326be9eb15dd02f3a804541ec3494168f30d234
Dean <deanr@live.de>
2021-01-27 13:43:49 +0100

    States

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { getData } from "./scripts/data.js"
 import Archetypes from "./archetypes.js";
 import Details from "./details.js";
 
@@ -6,18 +7,30 @@ class Layout extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            users: {},
             selectedUser: null
         }
     }
+
+    componentDidMount() {
+        console.log("LAYOUT: Did Mount")
+        getData().then((data) => this.setState({ users: data }));
+    }
+    componentDidUpdate() {
+        console.log("LAYOUT: Layout Updated")
+    }
+
     render() {
         return (
-            < div class="grid grid-cols-2 gap-4">
-                <div class=" bg-gray-100 rounded-md">
-                    <Archetypes onHighlight={(u) => this.setState({ selectedUser: u })} />
+            < div className="grid grid-cols-2 gap-4">
+                <div className=" bg-gray-100 rounded-md">
+                    <Archetypes users={this.state.users} onUserSelected={(u) => this.setState({ selectedUser: u })} />
                 </div>
-                <div class=" bg-gray-100 rounded-md">
+                <div className=" bg-gray-100 rounded-md">
                     <Details username={this.state.selectedUser} />
                 </div>
+                <div className=" bg-gray-100 rounded-md">Soon</div>
+                <div className=" bg-gray-100 rounded-md">Soon</div>
             </div >
         );
     }
